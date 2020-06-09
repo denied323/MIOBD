33affa0057aa32809f321304d2a8a47d1a31a80c
student3.uph.zl <student3uphzl>
2020-06-09 16:24:40 +0200

    formularz wyświetlający samochód oraz typ i pojemnosc silnika tego samochodu

--- src/main/java/com/example/miodb/controller/WyszukajController.java
+++ src/main/java/com/example/miodb/controller/WyszukajController.java
@@ -3,16 +3,13 @@ package com.example.miodb.controller;
 
 import com.example.miodb.model.*;
 import com.example.miodb.repository.SamochodRepository;
-import org.neo4j.ogm.annotation.Relationship;
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -25,33 +22,82 @@ public class WyszukajController {
     @Autowired
     private SamochodRepository samochodRepository;
 
-    @RequestMapping(value="/", method = RequestMethod.GET)
-    public String pokazOpcje(Model model){
-
-
-//        @Relationship(type="Stan", direction = Relationship.INCOMING) private List<Stan> stan;
-//        @Relationship(type="PojemnoscSilnika", direction = Relationship.INCOMING) private List<PojemnoscSilnika> pojemnoscSilnika;
-//        @Relationship(type="TypSilnika", direction = Relationship.INCOMING) private List<TypSilnika> typSilnika;
-//        @Relationship(type="TypUbezpieczenia", direction = Relationship.INCOMING) private List<TypUbezpieczenia> typUbezpieczenia;
-
+    @RequestMapping(value="/", method = {RequestMethod.GET})
+    public String wybierzSamochod(Model model){
 
         List<String> samochody = samochodRepository.getAllSamochod();
-
+        model.addAttribute("czyWybranoSamochod", false);
 
         model.addAttribute("samochody", samochody);
         return "index";
     }
 
 
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public String wybierzOpcje(Model model, @Valid @ModelAttribute("formularz") Formularz formularz, @RequestParam("samochod") String car){
 
-    @RequestMapping(value="/", method = RequestMethod.POST)
-    public String pokazNajUbezpieczyciela(Model model, @Valid @ModelAttribute("formularz") Formularz formularz){
+        if(car==null || car==""){
+            List<String> samochody = samochodRepository.getAllSamochod();
+            model.addAttribute("samochody", samochody);
+            model.addAttribute("czyWybranoSamochod", false);
+        }else{
+            List<String> samochody = samochodRepository.getAllSamochod();
+            model.addAttribute("samochody", samochody);
+            model.addAttribute("czyWybranoSamochod", true);
+            model.addAttribute("samochod", car);
 
 
-        String asd = null;
+
+
+            List<String> typSilnika = samochodRepository.getAllTypSilnika(car);
+
+
+
+
+            List<String> pojemnoscSilnika = samochodRepository.getAllPojemnoscSilnika(car);
+
+            final Logger logger = LoggerFactory.getLogger(WyszukajController.class);
+
+
+            logger.info("\n\n===========================================\n\n POJEMNOSC \n\n======================================");
+            for(int i =0; i<pojemnoscSilnika.size(); i++){
+                logger.info(pojemnoscSilnika.get(i));
+            }
+
+            logger.info("\n\n===========================================\n\n TYP \n\n======================================");
+            for(int i =0; i<typSilnika.size(); i++){
+                logger.info(typSilnika.get(i));
+            }
+
+
+
+            model.addAttribute("typSilnika", typSilnika);
+            model.addAttribute("pojemnoscSilnika", pojemnoscSilnika);
+
+        }
+
+
+
 
         return "index";
     }
+
+
+    @RequestMapping(value="/index2.html", method = {RequestMethod.GET, RequestMethod.POST})
+    public String test(Model model, @RequestParam("car") String car){
+
+
+
+
+        return "index2";
+    }
+
+
+
+
+
+
+
 
 
 }
--- src/main/java/com/example/miodb/repository/SamochodRepository.java
+++ src/main/java/com/example/miodb/repository/SamochodRepository.java
@@ -3,6 +3,7 @@ package com.example.miodb.repository;
 import com.example.miodb.model.Samochod;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,6 +11,31 @@ public interface SamochodRepository extends Neo4jRepository<Samochod, Long>{
 
     @Query("MATCH (a:Samochod) return a.nazwa")
     List<String> getAllSamochod();
+
+    @Query("MATCH (a:Samochod)-[c:PojemnoscSilnika]->(b:Ubezpieczyciel) \n" +
+            "where a.nazwa = $phrase " +
+            "return DISTINCT c.pojemnoscSilnika " +
+            "order by c.pojemnoscSilnika ASC")
+    List<String> getAllPojemnoscSilnika(@Param("phrase") String phrase);
+
+    @Query("MATCH (a:Samochod)-[c:TypSilnika]->(b:Ubezpieczyciel) " +
+            "where a.nazwa = $phrase " +
+            "return DISTINCT c.typSilnika")
+    List<String> getAllTypSilnika(@Param("phrase") String phrase);
+
+    @Query("match (a:Samochod)-[d:Stan]->(b:Ubezpieczyciel), " +
+            "(a)-[e:PojemnoscSilnika]->(b), " +
+            "(a)-[f:TypSilnika]->(b), " +
+            "(a)-[g:TypUbezpieczenia]->(b) " +
+            "where a.nazwa = $nazwaSamochodu AND d.stan = $nazwaStanu AND e.pojemnoscSilnika = $nazwaPojemnoscSilnika AND f.typSilnika = $nazwaTypSilnika AND g.stan = $nazwaTypuUbezpieczenia " +
+            "return b.nazwa as ubezpieczyciel, a.cena*(d.cena + e.cena + f.cena + g.cena) as cena " +
+            "order by cena ASC limit 1")
+    List<String> findBestUbezpieczenie(@Param("nazwaSamochodu") String nazwaSamochodu, @Param("nazwaStanu") String nazwaStanu, @Param("nazwaPojemnoscSilnika") String nazwaPojemnoscSilnika, @Param("nazwaTypSilnika") String nazwaTypSilnika, @Param("nazwaTypuUbezpieczenia") String nazwaTypuUbezpieczenia);
+
+
+
+    @Query("MATCH (target:B) <-[c:PARENT]- (child:B) WHERE target.play={Play} RETURN DISTINCT target.your_identity_property")
+    Long findActiveByPlay(@Param("Play") String play);
 
 
 }
